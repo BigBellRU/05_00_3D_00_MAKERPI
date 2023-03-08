1ddba33881270b5dd4a9d1da4b97f77d71f3729c
Dmitry Belov <Dmitry_Belov@mail.ru>
2023-03-08 23:50:42 +0300

    Chenge CUSTOM_MACHINE_NAME

--- Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
+++ Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
@@ -145,7 +145,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3"
+#define CUSTOM_MACHINE_NAME "05_00_3D_00_MAKERPI"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
