fc2052620ef74de161335026b6e0a0d24aec4f3a
Dmitry Belov <Dmitry_Belov@mail.ru>
2023-03-10 21:29:45 +0300

    Bug fix

--- Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration_adv.h
+++ Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration_adv.h
@@ -2496,7 +2496,7 @@
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       650      // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  (X_CURRENT/2)  // (mA) RMS current for sensorless homing
-    #define X_MICROSTEPS     64        // 0..256
+    #define X_MICROSTEPS   64
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1        // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ...
     //#define X_INTERPOLATE  true      // Enable to override 'INTERPOLATE' for the X axis
