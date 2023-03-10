6bb02b7ed88346f999a817b9e0c68da8b1441322
BigBellRU <vladimirebelov@mail.ru>
2023-03-10 17:27:15 +0300

    change MACHINE_NAME

--- Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
+++ Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
@@ -145,7 +145,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "MAKERPI"
+#define CUSTOM_MACHINE_NAME "MAKERPI P2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
